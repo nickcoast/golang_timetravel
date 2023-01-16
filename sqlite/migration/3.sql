ff2fafe09a68d0335f6d3753548964a944043ad9
nickcoast <nick@ogredata.com>
2023-01-15 19:33:52 -0800

    Add more records to DB initial state

--- sqlite/migration/3.sql
+++ sqlite/migration/3.sql
@@ -2,8 +2,9 @@ CREATE TABLE "insured_addresses" (
 	"id"	INTEGER NOT NULL,
 	"address"	TEXT NOT NULL,
 	"insured_id"	INTEGER NOT NULL,
-	"record_timesetamp"	INTEGER NOT NULL,
+	"record_timestamp"	INTEGER NOT NULL,
 	CONSTRAINT "insured_addresses_insured_id_address_unq" UNIQUE("insured_id","address"),
+	FOREIGN KEY(insured_id) REFERENCES insured(id)
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
@@ -11,6 +12,7 @@ CREATE TABLE "insured_addresses" (
 INSERT INTO insured_addresses
 (address, insured_id, record_timestamp)
 VALUES
-('123 Fake Street, Springfield, Oregon', 1, 1673687536),
-('123 REAL Street, Springfield, Oregon', 1, 1673687537),
-('Flavortown', 1, 1673834152)
+('123 Fake Street, Springfield, Oregon', 1, CAST(strftime('%s','1984-10-31 12:00:00') AS INT)),
+('123 REAL Street, Springfield, Oregon', 1, CAST(strftime('%s','1984-11-15 12:00:01') AS INT)),
+('Flavortown', 1, CAST(strftime('%s','1996-01-02 11:59:49') AS INT)),
+('Mars', 1, CAST(strftime('%s','1997-01-02 12:00:01') AS INT))
