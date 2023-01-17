f9e22246935b639e210655db536d92528acd5ec6
nickcoast <nick@ogredata.com>
2023-01-17 04:40:23 -0800

    Consolidate database migration files into one

--- sqlite/migration/1.sql
+++ sqlite/migration/1.sql
@@ -21,11 +21,39 @@ CREATE TABLE IF NOT EXISTS "employees" (
 	
 );
 
+INSERT INTO insured (name, policy_number, record_timestamp)
+VALUES
+('Jimmy Temelpa', 1000, CAST(strftime('%s','1984-10-31 12:00:00') AS INT)),
+('John Smith', 1001, CAST(strftime('%s','1999-12-31 23:59:59') AS INT));
 
-/* Assuming Sqlite will enforce NOT NULL, this won't be needed */
-/* CREATE TRIGGER employees_end_date
-AFTER INSERT ON employees
-BEGIN UPDATE employees
-SET end_date = "0001-01-01"
-WHERE NEW.end_date IS NULL;
-END */
+
+INSERT INTO employees (name, start_date, end_date, insured_id, record_timestamp)
+VALUES
+/* 0001-01-01 instead of NULL so UNIQUE constraint works */
+('Jimmy Temelpa', '1984-10-01', '0001-01-01', 1, CAST(strftime('%s','1984-10-31 12:00:00') AS INT)), 
+('Mister Bungle', '1984-11-10', '0001-01-01', 1, CAST(strftime('%s','1984-11-15 12:00:00') AS INT)),
+('Mister Bungle', '1984-11-10', '1996-01-02', 1, CAST(strftime('%s','1996-01-02 12:00:00') AS INT)), /* TIMETRAVEL */
+('Mister Bungle', '1984-11-10', '1996-06-01', 1, CAST(strftime('%s','1997-01-02 12:00:00') AS INT)), /* TIMETRAVEL */
+('John Smith', '1985-05-15', '1999-12-25', 2, CAST(strftime('%s','1999-12-31 23:59:59') AS INT)),
+('Jane Doe','1985-05-15', '1999-12-25',2, CAST(strftime('%s','2000-04-01 12:00:00') AS INT)),
+('Grant Tombly','1985-05-15', '1999-12-25',2, CAST(strftime('%s','2000-04-01 12:00:00') AS INT));
+
+
+CREATE TABLE "insured_addresses" (
+	"id"	INTEGER NOT NULL,
+	"address"	TEXT NOT NULL,
+	"insured_id"	INTEGER NOT NULL,
+	"record_timestamp"	INTEGER NOT NULL,
+	CONSTRAINT "insured_addresses_insured_id_address_unq" UNIQUE("insured_id","address"),
+	FOREIGN KEY(insured_id) REFERENCES insured(id) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+INSERT INTO insured_addresses
+(address, insured_id, record_timestamp)
+VALUES
+('123 Fake Street, Springfield, Oregon', 1, CAST(strftime('%s','1984-10-31 12:00:00') AS INT)),
+('123 REAL Street, Springfield, Oregon', 1, CAST(strftime('%s','1984-11-15 12:00:01') AS INT)),
+('Flavortown', 1, CAST(strftime('%s','1996-01-02 11:59:49') AS INT)),
+('Mars', 1, CAST(strftime('%s','1997-01-02 12:00:01') AS INT))
